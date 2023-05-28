9bb3bb3cdbb9731873e6942e171277e54b2264ba
Lore5032 <133913718+Lore5032@users.noreply.github.com>
2023-05-28 17:57:42 -0500

    Update sound.js

--- sound.js
+++ sound.js
@@ -13,4 +13,4 @@ soundEffect2.volume = volumeLevel;
 soundEffect1.play();
 setTimeout(function() {
   soundEffect2.play();
-}, 400); // Delay of 0.5 seconds (500 milliseconds)
+}, 350); // Delay of 0.5 seconds (500 milliseconds)
